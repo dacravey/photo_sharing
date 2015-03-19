7c994ebb875119ebbba94d45af502aedc337df8f
Debbie Cravey and Carli Martinez <cinemaestro@yahoo.com>
2015-03-19 11:24:24 -0700

    add ability to display user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   validates_with AttachmentPresenceValidator, :attributes => :avatar
   validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
     def encrypt_password
