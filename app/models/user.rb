6ae529670b0e3fd0b51cb70c833cfb5a0d0cf235
Debbie Cravey and Carli Martinez <cinemaestro@yahoo.com>
2015-03-19 11:09:22 -0700

    add avatar attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   attr_accessor :password
   validates_confirmation_of :password
   before_save :encrypt_password
+  validates :avatar, :attachment_presence => true
+  validates_with AttachmentPresenceValidator, :attributes => :avatar
+  validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
